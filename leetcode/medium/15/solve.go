14b16e94954499dda828725b38e36f898665918e
zhangli2946 <zhangli2946@gmail.com>
2021-03-20 23:40:13 +0800

    update m 15

--- leetcode/medium/15/solve.go
+++ leetcode/medium/15/solve.go
@@ -2,6 +2,38 @@ package _15
 
 import "sort"
 
+func threeSum3(nums []int) [][]int {
+    sort.Ints(nums)
+    var (
+        lon = len(nums)
+        ans = map[[3]int]byte{}
+        ret = make([][]int, 0)
+    )
+
+    for i := range nums {
+        if i > 0 && nums[i] == nums[i-1] {
+            continue
+        }
+        for j, k := i+1, lon-1; k > j; {
+            n := nums[i] + nums[j] + nums[k]
+            switch {
+            case n > 0:
+                k -= 1
+            case n < 0:
+                j += 1
+            default:
+                key := [3]int{nums[i], nums[j], nums[k]}
+                if _, found := ans[key]; !found {
+                    ans[key] = 1
+                    ret = append(ret, []int{nums[i], nums[j], nums[k]})
+                }
+                j, k = j+1, k-1
+            }
+        }
+    }
+    return ret
+}
+
 // 40 ms	7.2 MB
 func threeSum(nums []int) [][]int {
     sort.Ints(nums)
@@ -35,6 +67,7 @@ func threeSum(nums []int) [][]int {
     }
     return ret
 }
+
 // 48 ms	7 MB
 // convert ans  for range big key map has cost!
 func threeSum2(nums []int) [][]int {
@@ -64,7 +97,7 @@ func threeSum2(nums []int) [][]int {
     }
     ret := make([][]int, 0, len(ans))
     for k := range ans {
-        ret = append(ret, []int{k[0],k[1],k[2]})
+        ret = append(ret, []int{k[0], k[1], k[2]})
     }
     return ret
 }
